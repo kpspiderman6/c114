25baf22474acfe6e3b80e481c017c6b43869433f
kpspiderman6 <73602349+kpspiderman6@users.noreply.github.com>
2021-04-02 16:00:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function setup(){
+   canvas = createCanvas(300,300) ;
+   canvas.center();
+}
+function preload(){
+
+}
+
+function draw(){
+
+}
+function snap(){
+    save('my_snapshot.png');
+    
+}
